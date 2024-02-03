507dc33d3086c1c4e932469170bcf668e11fd65d
Zyraxxz <zyraxz@outlook.com>
2024-02-03 12:31:06 +0100

    First Example created
    - Added basic config
    Next Steps:
    - Change config items to something else

--- runelite-client/src/main/java/net/runelite/client/plugins/configexample/ConfigExampleConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/configexample/ConfigExampleConfig.java
@@ -1,4 +1,19 @@
 package net.runelite.client.plugins.configexample;
 
-public class ConfigExampleConfig {
+//Mandatory Imports
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("configexample")
+public interface ConfigExampleConfig extends Config {
+    @ConfigItem(
+            position = 1,
+            keyName = "exampleconfig",
+            name = "Welcome Greeting",
+            description = "The message to show to the user when they login"
+    )
+    default String greeting(){
+        return "Hello";
+    }
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/configexample/ConfigExamplePlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/configexample/ConfigExamplePlugin.java
@@ -1,4 +1,53 @@
 package net.runelite.client.plugins.configexample;
 
-public class ConfigExamplePlugin {
+import com.google.inject.Provides;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.ChatMessageType;
+import net.runelite.api.Client;
+import net.runelite.api.GameState;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Example"
+)
+public class ConfigExamplePlugin extends Plugin
+{
+    @Inject
+    private Client client;
+
+    @Inject
+    private ConfigExampleConfig config;
+
+    @Override
+    protected void startUp() throws Exception
+    {
+        log.info("Johns Config Example Started");
+    }
+
+    @Override
+    protected void shutDown() throws Exception
+    {
+        log.info("Johns Config Example Ended");
+    }
+
+    @Subscribe
+    public void onGameStateChanged(GameStateChanged gameStateChanged)
+    {
+        if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+        {
+            client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "John says " + config.greeting(), null);
+        }
+    }
+
+    @Provides
+    ConfigExampleConfig provideConfig(ConfigManager configManager)
+    {
+        return configManager.getConfig(ConfigExampleConfig.class);
+    }
 }
